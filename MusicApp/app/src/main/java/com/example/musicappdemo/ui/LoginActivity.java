8f293d06bd7baf679eb9a65f3e712eb45cbb2459
nguyensang17032006 <nguyensang17032006@gmail.com>
2026-07-09 13:29:17 +0700

    hoàn thành login

--- MusicApp/app/src/main/java/com/example/musicappdemo/ui/LoginActivity.java
+++ MusicApp/app/src/main/java/com/example/musicappdemo/ui/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.musicappdemo.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,38 +13,85 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.musicappdemo.MainActivity;
 import com.example.musicappdemo.R;
+import com.example.musicappdemo.data.RetrofitClient;
+import com.example.musicappdemo.data.SessionManager;
+import com.example.musicappdemo.databinding.ActivityLoginBinding;
+import com.example.musicappdemo.model.auth.AuthRequest;
+import com.example.musicappdemo.model.auth.AuthResponse;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginActivity extends AppCompatActivity {
-
-    TextInputEditText edtEmail, edtPassword;
-    MaterialButton btnLogin;
-    TextView  tvForgotPassword, tvGoRegister;
-
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.edtEmail.getText().toString().trim();
+                String password = binding.edtPassword.getText().toString().trim();
 
-        btnLogin = findViewById(R.id.btnLogin);
-        tvForgotPassword = findViewById(R.id.txtForgotPassword);
-        tvGoRegister = findViewById(R.id.txtGoRegister);
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                AuthRequest authRequest = new AuthRequest(email,password);
 
-        tvForgotPassword.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-            startActivity(intent);
+                RetrofitClient.getApiService().loginUser(authRequest).enqueue(new Callback<AuthResponse>() {
+                    @Override
+                    public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            AuthResponse authResponse = response.body();
+
+                            // Lưu lại phiên làm việc (Token) vào SessionManager để lần sau không bắt đăng nhập lại
+                            SessionManager.get(LoginActivity.this).save(authResponse);
+
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+
+                            // Bế User vào MainActivity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed: " + response.message(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<AuthResponse> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         });
 
-        tvGoRegister.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-            startActivity(intent);
+        binding.txtGoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
 
-        btnLogin.setOnClickListener(v -> {
-            // Viết code đăng nhập của cậu ở đây
+        binding.txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
